82fc52d30e153cc3485ceeaebb82c50d4104a9c4
Vijitha Samarasinghe (Lahanda Wadu) <74479248+vijitdesilva@users.noreply.github.com>
2021-07-07 00:50:59 -0400

    Schema

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE "Train_Top_Ten_Features" (
+firstflrsf INT, 
+GrLivArea INT, 
+LotArea INT, 
+GarageArea INT, 
+BsmtUnfSF INT, 
+TotalBsmtSF INT, 
+LotFrontage INT, 
+GarageYrBlt INT, 
+MoSold INT, 
+YearBuilt INT)
+
+SELECT* FROM Test_Top_Ten_Features;
+
+CREATE TABLE "Test_Top_Ten_Features" (
+firstflrsf INT, 
+GrLivArea INT, 
+LotArea INT, 
+GarageArea INT, 
+BsmtUnfSF INT, 
+TotalBsmtSF INT, 
+LotFrontage INT, 
+GarageYrBlt INT, 
+MoSold INT, 
+YearBuilt INT)
+
+SELECT* FROM Test_Top_Ten_Features;
