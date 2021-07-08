065a11e3b54c7e37d39e43e1425f5719d60b1fa7
lguloy <ljguloy@gmail.com>
2021-07-08 19:04:24 -0400

    config file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import (
 import model
 import os
 from tables import create_classes
-
+from config import password
 
 #################################################
 # Flask Setup
@@ -21,7 +21,7 @@ app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 
 ## CREATE CONNECTION ##
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:House2021@housingpriceprediction.cxgg7v5earry.us-east-2.rds.amazonaws.com:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or f'postgresql+psycopg2://postgres:{password}@housingpriceprediction.cxgg7v5earry.us-east-2.rds.amazonaws.com:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Features = create_classes(db)
